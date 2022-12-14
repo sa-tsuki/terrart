19e87e26fb939c4aaf3dd059cd55d6acc7060337
_sa_tsuki <isobe@plusjam.jp>
2022-12-14 21:09:57 +0900

    fix advanced level 2

--- public/progrative/advanced/level2/assets/js/index.js
+++ public/progrative/advanced/level2/assets/js/index.js
@@ -2,7 +2,6 @@
 // 定義
 //
 const actionOpen = document.querySelectorAll(".actionOpen");
-const originalHeight = 76;
 
 //
 // 関数
@@ -10,20 +9,21 @@ const originalHeight = 76;
 const openAccrodion = (e) => {
   const target = e.currentTarget;
   const parent = target.parentNode;
+  const hiddenBlock = parent.querySelector(".main__descBlock");
 
   if (parent.classList.contains("on")) {
     parent.classList.remove("on");
-    parent.style.height = `${originalHeight}px`;
+    hiddenBlock.style.height = `0`;
   } else {
     // 一度全てのonを消す
     actionOpen.forEach((elem) => {
       elem.parentNode.classList.remove("on");
-      elem.parentNode.style.height = `${originalHeight}px`;
+      elem.nextElementSibling.style.height = `0`;
     });
 
     parent.classList.add("on");
     const hiddenHight = parent.querySelector(".main__listDesc").offsetHeight;
-    parent.style.height = `${originalHeight + hiddenHight}px`;
+    hiddenBlock.style.height = `${hiddenHight}px`;
   }
 };
 //
